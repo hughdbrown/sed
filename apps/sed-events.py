d5b4f96c29caf61b4ae492c14a51080ae83f9c1a
Hugh Brown <hbrown@wgen.net>
2013-05-02 22:41:26 -0400

    ./apps/sed-events.py: E502 the backslash is redundant between brackets

--- apps/sed-events.py
+++ apps/sed-events.py
@@ -47,7 +47,7 @@ class StreamEditorModifyEventsWithinMethod(StreamEditor):
                 new_lines = build_pairs([
                     (ve["selector"], ve["function"])
                     for ve in var_events])
-                loc = (var_events[0]["line_no"], \
+                loc = (var_events[0]["line_no"],
                     var_events[-1]["line_no"] + 1)
                 self.replace_range(loc, new_lines)
                 self.entab()
