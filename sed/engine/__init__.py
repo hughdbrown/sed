39eea826a1f29c2bd77d5f4f5bead7011b47f0bb
Hugh Brown <hughdbrown@yahoo.com>
2018-01-10 13:00:29 -0700

    Add CUT to list of externally visible objects

--- sed/engine/__init__.py
+++ sed/engine/__init__.py
@@ -1,12 +1,22 @@
+"""
+Interface to sed engine
+- defines objects exported from this module
+"""
 from sed.engine.StreamEditor import StreamEditor
 from sed.engine.sed_file_util import call_main
-from sed.engine.match_engine import ACCEPT, REJECT, NEXT, REPEAT, CUT
+from sed.engine.match_engine import (
+    ACCEPT,
+    REJECT,
+    NEXT,
+    REPEAT,
+    CUT,
+)
 from sed.engine.sed_regex import ANY
 
 
 __all__ = [
     "StreamEditor",
     "call_main",
-    "ACCEPT", "REJECT", "NEXT", "REPEAT",
+    "ACCEPT", "REJECT", "NEXT", "REPEAT", "CUT",
     "ANY",
 ]
